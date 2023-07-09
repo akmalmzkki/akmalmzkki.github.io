63b638ad49118a9b263c2719bada8877c248e3ba
akmalmzkki <akmalmzkki@student.telkomuniversity.ac.id>
2023-07-09 14:36:23 +0700

    first commit

---
+++ static/js/mailer.js
@@ -0,0 +1,27 @@
+// var nodemailer = require('nodemailer');
+
+// var transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: 'akmalm2003@gmail.com',
+//         pass: 'mudzacky2583'
+//     }
+// });
+
+// var mailOptions = {
+//     // Input dari html
+//     from: 'akmalmudzackye@gmail.com',
+//     to: 'akmalm2003@gmail.com',
+//     subject: 'oke',
+//     text: 'oke'
+// };
+
+// transporter.sendMail(mailOptions, function (error, info) {
+//     if (error) {
+//         console.log(error);
+//         res.send('error');
+//     } else {
+//         console.log('Email sent: ' + info.response);
+//         res.send('success');
+//     }
+// });
---
+++ static/js/script.js
@@ -0,0 +1,74 @@
+AOS.init();
+
+$("a").on('click', function(event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+        let hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function(){
+            window.location.hash = hash;
+        });
+    }
+});
+
+$(window).scroll(function(){
+    if ($(window).scrollTop() > screenTop) {
+        $("nav").addClass("scrolled");
+    } else {
+        $("nav").removeClass("scrolled");
+    }
+});
+
+const btndark = $(".btn-dark")
+const btnlight = $(".btn-light")
+const projectbtn = $(".project-btn")
+
+btndark.hover(function() {
+    $(this).children("svg").children("path").attr("stroke", "#28293D")
+    $(this).children("svg").attr("fill", "#28293D")
+
+});
+btndark.mouseout(function() {
+    $(this).children("svg").children("path").attr("stroke","white")
+    $(this).children("svg").attr("fill", "white")
+
+});
+
+btnlight.hover(function() {
+    $(this).children("svg").children("path").attr("stroke","white")
+});
+btnlight.mouseout(function() {
+    $(this).children("svg").children("path").attr("stroke","#28293D")
+});
+
+projectbtn.hover(function() {
+    $(this).children("svg").children("path").attr("stroke","#28293D")
+});
+projectbtn.mouseout(function() {
+    $(this).children("svg").children("path").attr("stroke","white")
+});
+
+$(".navbar-toggler").click(function() {
+    $(this).toggleClass("clicked");
+    $(".navbar-nav").toggleClass("show");
+});
+
+$(document).ready(function() {
+    $(".nav-link").hover(
+        function() {
+            $(this).find(".dropdown-content").slideDown(200);
+        },
+        function() {
+            $(this).find(".dropdown-content").slideUp(200);
+        }
+    );
+});
+
+if (window.location.protocol !== 'http:' && window.location.protocol !== 'https:') {
+    window.location.href = 'alert.html';
+}
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    alert('Silakan kunjungi website ini melalui perangkat desktop untuk pengalaman terbaik.');
+}
